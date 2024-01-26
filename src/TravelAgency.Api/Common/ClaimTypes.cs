9cb33ca15f0eccea430d0f0c6fb0e04bb8de5006
Javier Lima <javi.limashelby@gmail.com>
2024-01-26 11:35:01 -0500

    feat()[auth]: Add ClaimTypes

---
+++ src/TravelAgency.Api/Common/ClaimTypes.cs
@@ -0,0 +1,9 @@
+namespace TravelAgency.Api.Common;
+
+public static class ClaimTypes
+{
+    public const string GivenName = "givenname";
+    public const string Surname = "surname";
+    public const string NameIdentifier = "nameidentifier";
+    public const string Role = "role";
+}
