884566d3c8ac65352ffb07eb06b52d5d1ca76b29
Javier Lima <javi.limashelby@gmail.com>
2024-01-26 09:39:32 -0500

    fix()[auth]: Add public access modifier to User Entity

--- src/TravelAgency.Domain/Entities/User.cs
+++ src/TravelAgency.Domain/Entities/User.cs
@@ -2,9 +2,10 @@ namespace TravelAgency.Domain.Entities;
 
 public class User
 {
-    Guid Id { get; set; }
-    string Name { get; set; } = null!;
-    string LastName { get; set; } = null!;
-    string Email { get; set; } = null!;
-    string Role { get; set; } = null!;
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password {get; set;} = null!;
+    public string Role { get; set; } = null!;
 }
