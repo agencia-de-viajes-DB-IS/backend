68fb3442926743916e989277786cbfe0b3327ce6
Javier Lima <javi.limashelby@gmail.com>
2024-01-26 10:24:46 -0500

    feat()[auth]: Add Infrastructure Layer Dependency Injection
    - Add Authentication

--- src/TravelAgency.Api/Program.cs
+++ src/TravelAgency.Api/Program.cs
@@ -2,14 +2,17 @@ using FastEndpoints;
 using FastEndpoints.Swagger;
 using TravelAgency.Api;
 using TravelAgency.Application;
+using TravelAgency.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPresentation();
 builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseAuthentication();
 app.UseFastEndpoints();
 app.UseSwaggerGen();
 app.Run();
---
+++ src/TravelAgency.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,46 @@
+using System.Text;
+using TravelAgency.Application.Interfaces.Authentication;
+using TravelAgency.Application.Interfaces.Persistence;
+using TravelAgency.Infrastructure.Authentication;
+using TravelAgency.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TravelAgency.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfigurationManager configuration)
+    {
+        // services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        // services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SECTION_NAME));
+        services.AddAuth(configuration);
+        services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+        return services;
+    }
+
+    private static IServiceCollection AddAuth(this IServiceCollection services, IConfigurationManager configuration)
+    {
+        var jwtSettings = new JwtSettings();
+        configuration.Bind(JwtSettings.SECTION_NAME, jwtSettings);
+
+        services.AddSingleton(Options.Create(jwtSettings));
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+
+        services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings.Issuer,
+            ValidAudience = jwtSettings.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
+        });
+
+        return services;
+    }
+}
