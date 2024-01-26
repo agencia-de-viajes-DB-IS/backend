d96d85ff420f66eb645ba8ff70266bf781164e1e
Javier Lima <javi.limashelby@gmail.com>
2024-01-26 09:59:32 -0500

    feat()[auth]: Add Application Layer Dependency Injection

--- src/TravelAgency.Api/Program.cs
+++ src/TravelAgency.Api/Program.cs
@@ -1,10 +1,12 @@
 using FastEndpoints;
 using FastEndpoints.Swagger;
 using TravelAgency.Api;
+using TravelAgency.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPresentation();
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
