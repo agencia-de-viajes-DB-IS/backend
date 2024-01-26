58bc5861593382f12594f013b7af86d1f50ed75a
Javier Lima <javi.limashelby@gmail.com>
2024-01-26 10:16:25 -0500

    feat()[auth]: Implement JwtTokenGenerator

---
+++ src/TravelAgency.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,47 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TravelAgency.Application.Interfaces.Authentication;
+using TravelAgency.Domain.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TravelAgency.Infrastructure.Authentication;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    private const string Roles = "roles";
+    private readonly JwtSettings _jwtSettings;
+
+    public JwtTokenGenerator(IOptions<JwtSettings> jwtSettingsOptions)
+    {
+        _jwtSettings = jwtSettingsOptions.Value;
+    }
+
+    public string GenerateToken(User user)
+    {
+        var key = new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Secret)),
+            SecurityAlgorithms.HmacSha256
+        );
+
+        var claims = new Claim[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.GivenName, user.FirstName),
+            new Claim(JwtRegisteredClaimNames.FamilyName, user.LastName),
+            new Claim(Roles, user.Role),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var securityToken = new JwtSecurityToken(
+            signingCredentials: key,
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            expires: DateTime.UtcNow.AddMinutes(_jwtSettings.MinutesToExpire),
+            claims: claims
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(securityToken);
+    }
+}
