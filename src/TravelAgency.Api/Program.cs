d44f9c62e126f23cee49c5803dcb7c58d3e89f1e
Albaro Suarez Valdes <albarosuarez02@gmail.com>
2024-01-29 17:16:48 -0500

    fix()[develop]: set cors to allow any source

--- src/TravelAgency.Api/Program.cs
+++ src/TravelAgency.Api/Program.cs
@@ -9,9 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddPresentation();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddCors(options => {
+    options.AddPolicy("MyPolicy", builder => {
+    // TODO: set defaults origins.
+        builder.AllowAnyHeader();
+        builder.AllowAnyMethod();        
+    });
+});
 
 var app = builder.Build();
-
+app.UseCors("MyPolicy");
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseFastEndpoints();
