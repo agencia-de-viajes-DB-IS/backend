04c40f9ca6ef68e92468bea452d4a5fc28d287db
BritoAlv <gomzalezbrito@gmail.com>
2024-01-31 23:08:05 -0500

    improve .sql

--- src/TravelAgency.Infrastructure/database/AereoSkull.sql
+++ src/TravelAgency.Infrastructure/database/AereoSkull.sql
@@ -18,30 +18,11 @@ create table Tourist
     nationality varchar(255) not null
 );
 
-create table Agency_Works_With_Tourist
-(
-	IdA int, 
-	IdT int,
-    primary key ( IdA, IdT),
-	foreign key (IdA) references Agency(IdA),
-    foreign key (IdT) references Tourist(IdT)
-);
-
 create table Hotel
 (
 	IdH int primary key not null auto_increment,
     nameH varchar(255) not null, 
     category int
-);
-
-create table Hotel_Deals
-(
-	IdHD int not null auto_increment,
-    IdH int,
-    primary key (IdHD, IdH),
-    foreign key (IdH) references Hotel(IdH),
-    descriptionHD varchar(255),
-    priceHD decimal(15,2)
 );
 
 create table Groupp
@@ -50,13 +31,11 @@ create table Groupp
     nameG varchar(255)
 );
 
-create table Tourist_Group
+create table Facility
 (
-	IdTG int,
-    IdT int,
-    primary key (IdTG, IdT),
-    foreign key (IdTG) references Groupp(IdTG),
-    foreign key (IdT) references Tourist(IdT)
+	idF int primary key not null auto_increment,
+    nameF varchar(255),
+    descriptionF varchar(255)
 );
 
 create table Excursion
@@ -77,14 +56,22 @@ create table Excursion_Associated_Hotel
     foreign key(idH) references Hotel(idH)
 );
 
+create table Agency_Works_With_Tourist
+(
+	IdA int, 
+	IdT int,
+    primary key ( IdA, IdT),
+	foreign key (IdA) references Agency(IdA),
+    foreign key (IdT) references Tourist(IdT)
+);
+
 create table Agency_Works_With_Excursions
 (
 	IdA int, 
 	IdE int,
     primary key ( IdA, IdE),
 	foreign key (IdA) references Agency(IdA),
-    foreign key (IdE) references Excursion(IdE),
-    price_per_agency_per_excursion decimal(15, 2)
+    foreign key (IdE) references Excursion(IdE)
 );
 
 create table Agency_Works_With_Hotel
@@ -93,8 +80,26 @@ create table Agency_Works_With_Hotel
 	IdH int,
     primary key ( IdA, IdH),
 	foreign key (IdA) references Agency(IdA),
+    foreign key (IdH) references Hotel(IdH)
+);
+
+create table Hotel_Deals
+(
+	IdHD int not null auto_increment,
+    IdH int,
+    primary key (IdHD, IdH),
     foreign key (IdH) references Hotel(IdH),
-    price_per_agency_per_hotel decimal(15, 2)
+    descriptionHD varchar(255),
+    priceHD decimal(15,2)
+);
+
+create table Tourist_Group
+(
+	IdTG int,
+    IdT int,
+    primary key (IdTG, IdT),
+    foreign key (IdTG) references Groupp(IdTG),
+    foreign key (IdT) references Tourist(IdT)
 );
 
 create table Package
@@ -109,20 +114,15 @@ create table Package
     descriptionP varchar(255)
 );
 
-create table Facility
-(
-	idF int primary key not null auto_increment,
-    nameF varchar(255),
-    descriptionF varchar(255)
-);
-
 create table Package_Facilities
 (
 	IdF int, 
 	IdP int,
-    primary key ( IdF, IdP),
+	idE int,
+    primary key ( IdF, IdP, idE),
 	foreign key (IdF) references Facility(IdF),
-    foreign key (IdP) references Package(IdP)
+    foreign key (IdP) references Package(IdP),
+    foreign key (idE) references Package(IdE)
 );
 
 create table Group_Reservation
@@ -132,7 +132,9 @@ create table Group_Reservation
     reservation_date date,
     primary key( idA, idTG, reservation_date),
     idP int,
+    idE int,
     foreign key (idP) references Package(idP),
+    foreign key (idE) references Excursion(idE),
     foreign key (idA) references Agency(idA),
     foreign key (idTG) references Groupp(idTG),
     participants_size int,
@@ -146,7 +148,7 @@ create table Individual_Reservation
 	idA int,
     idT int,
     reservation_date date,
-    index(reservation_date),
+    index(reservation_date), # this allows use the data type as a foreign key. why ?
     primary key(idA, idT, reservation_date),
     idE int,
     foreign key (idA) references Agency(idA),
