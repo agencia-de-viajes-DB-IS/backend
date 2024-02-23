7493255f9d1183092041ba1c2255b7655ecd8283
Javier Lima <javi.limashelby@gmail.com>
2024-02-23 16:40:01 -0500

    fix()[domain_cuba]: Relate Tourists to Reservations

--- src/TravelAgency.Domain/Entities/Tourist.cs
+++ src/TravelAgency.Domain/Entities/Tourist.cs
@@ -2,8 +2,14 @@ namespace TravelAgency.Domain.Entities;
 
 public class Tourist
 {
-    public Guid Id {get; set;}
-    public required string  FirstName {get; set;}
-    public required string LastName {get; set;}
-    public required string Nationality {get; set;}
+    // Main Properties
+    public Guid Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Nationality { get; set; }
+
+    // Relational Properties
+    public required ICollection<PackageReservation> PackageReservations { get; set; }
+    public required ICollection<ExcursionReservation> ExcursionReservations { get; set; }
+    public required ICollection<HotelDealReservation> HotelDealReservations { get; set; }
 }
