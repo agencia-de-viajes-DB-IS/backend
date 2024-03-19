7eb6ef8b68f1abcaa311f151b24186ceacaf06dd
Javier Lima <javi.limashelby@gmail.com>
2024-03-19 10:07:37 -0400

    feat()[feat/packages]: Implement UpdatePackage feature

---
+++ src/TravelAgency.Application/Handlers/Packages/UpdatePackage/UpdatePackageCommandValidator.cs
@@ -0,0 +1,21 @@
+using FluentValidation;
+using TravelAgency.Application.Handlers.Packages.UpdatePackage;
+using TravelAgency.Application.Interfaces.Persistence;
+namespace TravelAgency.Application.Handlers.Packages.CreatePackage;
+
+public class UpdatePackageCommandValidator : AbstractValidator<UpdatePackageCommand>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public UpdatePackageCommandValidator(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+
+        // Validation process
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required")
+            .MaximumLength(200).WithMessage("Description must not exceed 200 characters");
+        RuleFor(x => x.Price)
+            .GreaterThan(0)
+            .WithMessage("Price must be greater than 0");
+    }
+}
