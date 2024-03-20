7a5152c7b2f312b1b61ccd606b95e1bed8db79c1
Javier Lima <javi.limashelby@gmail.com>
2024-03-20 11:16:10 -0400

    feat()[feat/facilities]: Implement DeleteFacility feature

---
+++ src/TravelAgency.Api/Features/Facility/DeletFacilityEndpoint.cs
@@ -0,0 +1,20 @@
+using FastEndpoints;
+using MediatR;
+using TravelAgency.Application.Handlers.Facilities.DeleteFacility;
+
+namespace TravelAgency.Api.Features.Facility;
+
+public class DeletFacilityEndpoint(ISender _mediator) : Endpoint<DeleteFacilityCommand, DeleteFacilityResponse>
+{
+    public override void Configure()
+    {
+        Delete("/facilities");
+        // TODO: This cannot remain anonymous. Only authorized and with specified permission can access this endpoint
+        AllowAnonymous();
+    }
+    public override async Task HandleAsync(DeleteFacilityCommand command, CancellationToken ct)
+    {
+        var response = await _mediator.Send(command, ct);
+        await SendOkAsync(response, ct);
+    }
+}
