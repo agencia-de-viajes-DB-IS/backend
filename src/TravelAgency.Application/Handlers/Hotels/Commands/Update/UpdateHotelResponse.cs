049a04abef8e9c933af5a348b2820e5400650533
Albaro Suarez Valdes <albarosuarez02@gmail.com>
2024-03-22 12:01:43 -0400

    feat()[feat_hotel]: add update hotel endpoint

---
+++ src/TravelAgency.Application/Handlers/Hotels/Commands/Update/UpdateHotelResponse.cs
@@ -0,0 +1,7 @@
+using TravelAgency.Application.Responses;
+namespace TravelAgency.Application.Handlers.Hotels.Commands.Update;
+
+public class UpdateHotelResponse : BaseResponse
+{
+    public Guid Id {get; set;}
+};
