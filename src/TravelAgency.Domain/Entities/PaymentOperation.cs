cc2f68a104c67e7198de66a291166db49682f315
Javier Lima <javi.limashelby@gmail.com>
2024-03-29 14:58:42 -0400

    feat()[develop]: Add PaymentOperation entity

---
+++ src/TravelAgency.Domain/Entities/PaymentOperation.cs
@@ -0,0 +1,15 @@
+using TravelAgency.Domain.Enums;
+
+namespace TravelAgency.Domain.Entities;
+
+public class PaymentOperation
+{
+    // Main Properties
+    public Guid Id { get; set; }
+    public required string InternalPaymentId { get; set; }
+    public required string ExternalPaymentId { get; set; }
+    public required string Description { get; set; }
+    public required string ProductsInfoSerializedJson { get; set; }
+    public Currency Currency {get; set;}
+    public PaymentType PaymentType {get; set;}
+}
---
+++ src/TravelAgency.Domain/Enums/Currency.cs
@@ -0,0 +1,3 @@
+namespace TravelAgency.Domain.Enums;
+
+public enum Currency { }
