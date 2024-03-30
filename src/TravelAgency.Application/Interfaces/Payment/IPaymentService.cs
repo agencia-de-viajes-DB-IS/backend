0b7c36dd666a52a554820661742eb1d02c454484
Albaro Suarez Valdes <albarosuarez02@gmail.com>
2024-03-30 10:56:31 -0400

    feat()[payment_service]: add implementation for payment service

--- src/TravelAgency.Application/Interfaces/Payment/IPaymentService.cs
+++ src/TravelAgency.Application/Interfaces/Payment/IPaymentService.cs
@@ -17,21 +17,32 @@ public class PaymentResponse : BaseResponse
 public class CreatePaymentRequest
 {
     public required IEnumerable<ProductData> Products;
-    public const string Currency = "usd";
-
+    public string Currency { get; set; } = "usd";
+    public required string SuccessUrl { get; set; }
+    public required string CancelUrl { get; set; }
 }
 public class ProductData 
 {
-    private double _price;
     public string Name { get; set; } = "No name"; 
-    public required double Price
+    private int _price;
+    public required int Quantity
     {
         get { return _price; }
         set { 
             _price = (value > 0)? value : 
+                throw new TravelAgencyException($"Invalid quantity for product {Name}","",400); 
+            }
+    }
+    private double _quantity;
+    public required double Price
+    {
+        get { return _quantity; }
+        set { 
+            _quantity = (value > 0)? value : 
                 throw new TravelAgencyException($"Invalid price for product {Name}","",400); 
             }
     }
-    public string[] Images { get; set; } = []; 
+    
+    public List<string> Images { get; set; } = []; 
     public string Description { get; set; } = "No description";
 }
