0bd329a3713cf9d1afeb66a999fcdc1afbeccd72
Javier Lima <javi.limashelby@gmail.com>
2024-04-02 00:06:41 -0400

    feat()[develop]: Update PaymentMethod with migration

---
+++ src/TravelAgency.Infrastructure/Migrations/20240402040458_Update PaymentOperation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelAgency.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePaymentOperation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "PaymentOperations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "PaymentOperations");
+        }
+    }
+}
--- src/TravelAgency.Infrastructure/Persistence/AeroSkullDbContextFactory.cs
+++ src/TravelAgency.Infrastructure/Persistence/AeroSkullDbContextFactory.cs
@@ -8,7 +8,7 @@ public class AeroSkullContextFactory : IDesignTimeDbContextFactory<AeroSkullDbCo
     public AeroSkullDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<AeroSkullDbContext>();
-        optionsBuilder.UseMySQL("Server=localhost;User ID=root;Password=BritoAlv.2002;Port=3306;Database=Aero_Skull");
+        optionsBuilder.UseMySQL(args[0]);
 
         return new AeroSkullDbContext(optionsBuilder.Options);
     }
