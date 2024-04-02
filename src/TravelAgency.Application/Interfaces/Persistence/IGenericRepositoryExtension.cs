4c4168976448104c727a675b61b3f363dcfd1fc7
BritoAlv <gomzalezbrito@gmail.com>
2024-04-01 21:56:13 -0400

    fix () [develop] delete unused tourist method

--- src/TravelAgency.Application/Interfaces/Persistence/IGenericRepositoryExtension.cs
+++ src/TravelAgency.Application/Interfaces/Persistence/IGenericRepositoryExtension.cs
@@ -6,35 +6,6 @@ namespace TravelAgency.Application.Interfaces.Persistence;
 
 public static class IGenericRepositoryExtension
 {
-    public static async Task<ICollection<Tourist>> StoreRequestTourists(this IGenericRepository<Tourist> touristRepo, IEnumerable<CreateTouristCommand> requestTourists)
-    {
-        var tourists = new List<Tourist>();
-
-        foreach (var requestTourist in requestTourists)
-        {
-            var storedTourist = await touristRepo.FindAsync(filters: [tourist => tourist.Id == requestTourist.Id]);
-
-            if (storedTourist is null)
-            {
-                var newTourist = new Tourist()
-                {
-                    Id = requestTourist.Id,
-                    FirstName = requestTourist.FirstName,
-                    LastName = requestTourist.LastName,
-                    Nationality = requestTourist.Nationality
-                };
-
-                await touristRepo.InsertAsync(newTourist);
-                tourists.Add(newTourist);
-            }
-            else
-            {
-                tourists.Add(storedTourist);
-            }
-        }
-
-        return tourists;
-    }
     public static async Task<int> AvailableCapacity(this IGenericRepository<Package> packageRepository, IGenericRepository<PackageReservation> packageReservationRepo, Guid packageCode)
     {
         var packageReservations = await packageReservationRepo.FindAllAsync(
